8ca4fc5bf7bca96996c187a10a8045a0ec4a4242
Joao C Franca <joaocariasdefranca@gmail.com>
2023-11-30 16:35:38 -0300

    criando entidade Usuario

--- Jcf.Estacionamento/Jcf.Estacionamento.Api/Entidades/Usuario.cs
+++ Jcf.Estacionamento/Jcf.Estacionamento.Api/Entidades/Usuario.cs
@@ -1,6 +1,26 @@
-﻿namespace Jcf.Estacionamento.Api.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jcf.Estacionamento.Api.Entidades
 {
     public class Usuario : EntidadeBase
     {
+        [Required]
+        [StringLength(255)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Senha { get; set; }
+
+        public Usuario(string nome, string email, string senha, Guid? usuarioCriacaoId = null) : base(usuarioCriacaoId)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+        }
     }
 }
